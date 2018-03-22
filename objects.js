03544598ccd43c1a67da63dd0ce3d524d2bbd0c8
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-03-22 13:35:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist ={oyna: 'tans'} ;
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName = gulalek;
+  playlist[artistName] = gulalek;
   return playlist;
 }
